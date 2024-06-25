1af39efb5b4360c4e6da8ba5fb76f3165a4ae7cd
adimerpaul <adimer101@gmail.com>
2024-06-24 20:14:46 -0400

    creado una version

---
+++ front/dist/pwa/assets/QSpace.2a615141.js
@@ -0,0 +1,1 @@
+import{h as e,c as a}from"./index.556a6fe6.js";const c=e("div",{class:"q-space"});var s=a({name:"QSpace",setup(){return()=>c}});export{s as Q};
--- front/dist/pwa/assets/QSpace.68fd497c.js
+++
@@ -1,1 +0,0 @@
-import{h as e,c as a}from"./index.050a4d8c.js";const c=e("div",{class:"q-space"});var s=a({name:"QSpace",setup(){return()=>c}});export{s as Q};
